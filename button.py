53ecf2d97433e038a2c9852b2a71c9dbe1645c1d
Fetter Dávid <fetter.david00@gmail.com>
2022-09-30 23:58:38 +0200

    Add hepling numbers

--- button.py
+++ button.py
@@ -1,11 +1,28 @@
 import pygame
 import os
+pygame.font.init()
 
 class Button():
 
-    def __init__(self,tableSize,windowWidth):
+    def __init__(self,tableSize,windowWidth,position,index):
+        self.position = position
+        self.index = index
         self.size = windowWidth // tableSize 
-        self.image = pygame.image.load(os.path.join("Assets","button.png"))
-        self.images = pygame.transform.scale(self.image,(self.size,self.size))
+        self.coverImg = pygame.transform.scale(pygame.image.load(os.path.join("Assets","button.png")),(self.size,self.size))
+        self.mineImg = pygame.transform.scale(pygame.image.load(os.path.join("Assets","buttonRed.png")),(self.size,self.size))
+        self.emptyImg = pygame.transform.scale(pygame.image.load(os.path.join("Assets","buttonGreen.png")),(self.size,self.size))
+        self.isMine = False
+        self.nearMines = 0
+        self.nearMinesText = pygame.font.SysFont("monospace", 50, bold=True)
+
+    def CheckForInput(self,mousePos):
+        if mousePos[0] in range(self.position[0],self.position[0]+self.size) and mousePos[1] in range(self.position[1],self.position[1]+self.size):
+            return True
+        return False
+
+    def SetToMine(self):
+        self.isMine = True
+
+
 
     
--- main.py
+++ main.py
@@ -1,48 +1,85 @@
 import pygame
 import button
+import random
 
-tableSize = 15
+tableSize = 10
+numberOfMine = 10
 
-HEIGHT = tableSize*50
-WIDTH = tableSize*50
+BUTTON_SIZE = 50
+HEIGHT = tableSize*BUTTON_SIZE
+WIDTH = tableSize*BUTTON_SIZE
 WIN = pygame.display.set_mode((WIDTH,HEIGHT))
 FPS = 60
 pygame.display.set_caption("Minesweeper")
+table = []
 
-def createTable(size):
-    table = []
+def CreateTable(size):
+    y = 0
     for i in range(size):
+        x = 0
         row = []
         for j in range(size):
-            row.append(button.Button(size,WIDTH)) 
-        table.append(row)
-
-    return table
-
-def drawTable():
-    table = createTable(tableSize)
-    
-    y = 0
-    for row in table:
-        x = 0
-        for button in row:
-            print(x)
-            WIN.blit(button.images,(x,y))
+            row.append(button.Button(size,WIDTH,[x,y],[i,j])) 
             x += WIDTH//tableSize
         y += HEIGHT//tableSize  
+        table.append(row)
+    
+    #Plant mines
+    mines = []
+    while len(mines) < numberOfMine:
+        mPos = [random.randint(0,tableSize-1),random.randint(0,tableSize-1)]
+        if(not table[mPos[0]][mPos[1]].isMine):
+            table[mPos[0]][mPos[1]].SetToMine()
+            mines.append( table[mPos[0]][mPos[1]])
+    
+    #Set helping numbers
+    for mine in mines:
+        for i in [-1,0,1]:
+            for j in [-1,0,1]:
+                pos = [mine.index[0]+i,mine.index[1]+j]
+                if( not (pos[0] < 0 or pos[0] >= size or pos[1] < 0 or pos[1] >= size) and not table[pos[0]][pos[1]].isMine):
+                    table[pos[0]][pos[1]].nearMines += 1
+
+
+def DrawTable():
+    CreateTable(tableSize)
+    
+    for row in table:
+        for button in row:
+            WIN.blit(button.coverImg,(button.position[0],button.position[1]))
 
     pygame.display.update()
+
+def ButtonCheck():
+    for row in table:
+        for button in row:
+            if button.CheckForInput(pygame.mouse.get_pos()):
+                if(button.isMine):
+                    WIN.blit(button.mineImg,(button.position[0],button.position[1]))
+                else:
+                    WIN.blit(button.emptyImg,(button.position[0],button.position[1]))
+                    if button.nearMines > 0:
+                        buttonText = button.nearMinesText.render(str(button.nearMines),1,(0,0,0))
+                        WIN.blit(buttonText,(button.position[0]+10,button.position[1]))
 
 def main():
     clock = pygame.time.Clock()
     run = True
-    drawTable()
+    DrawTable()
 
     while run:
         clock.tick(FPS)
         for evant in pygame.event.get():
+
             if evant.type == pygame.QUIT:
                 run = False
+            
+            if evant.type == pygame.MOUSEBUTTONDOWN:
+                ButtonCheck()
+
+        pygame.display.update()
+
+        
 
     pygame.quit() 
 
